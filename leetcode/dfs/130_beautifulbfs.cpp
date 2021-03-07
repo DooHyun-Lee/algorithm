5570d9b1792b554556fdab7d9163584fafff3dfb
DooHyun-Lee <62175406+DooHyun-Lee@users.noreply.github.com>
2021-03-07 16:31:15 +0900

    Create 130_beautifulbfs.cpp

---
+++ leetcode/dfs/130_beautifulbfs.cpp
@@ -0,0 +1,42 @@
+class Solution {
+ public:
+  void solve(vector<vector<char>>& board) {
+    if (board.empty())
+      return;
+    const int m = board.size();
+    const int n = board[0].size();
+    const vector<int> dirs{0,1,0,-1,0};
+    
+    queue<pair<int,int>> qu;
+    for(int i=0;i<m;i++){
+      for(int j=0;j<n;j++){
+        if(i*j ==0 || i == m-1 || j == n-1){
+          if(board[i][j] == 'O'){
+            qu.push(make_pair(i,j));
+            board[i][j] = '*';
+          }
+        }
+      }
+    }
+    while(!qu.empty()){
+      const auto [i,j] = qu.front();
+      qu.pop();
+      for(int k=0;k<4;k++){
+        const int x= i + dirs[k];
+        const int y= j + dirs[k+1];
+        if (x < 0 || x == m || y < 0 || y == n)
+         continue;
+        if (board[x][y] != 'O')
+          continue;
+        q.push({x, y});
+        board[x][y] = '*';
+      }
+    }
+     for (vector<char>& row : board)
+      for (char& c : row)
+        if (c == '*')
+          c = 'O';
+        else if (c == 'O')
+          c = 'X';
+  }
+};
